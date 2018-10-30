3203ae29a58c1b54297f831c05a5caa7f4379c6f
Marin Sapunar <marin.sapunar@gmail.com>
2018-10-30 13:48:26 +0100

    Read prog_overlap input directly from turbomole

--- src/prog_overlap.f90
+++ src/prog_overlap.f90
@@ -1,83 +1,89 @@
 program cis_olap_test
     use global_defs
     use cis_overlap_mod
-    use read_txt_mod
+    use read_all_mod
     use orthog_mod
-    use blas95, only : gemm, dot, gemv
+    use ccg_ao_mod
+    use one_el_op_mod
+    use matrix_mod
+    use occupation_mod
     implicit none
 
     integer :: rhf = 0 !< Restricted (1) or unrestricted(2) calculation.
     integer :: rhf1 = 0 !< Restricted (1) or unrestricted (2) for 1 wave functions.
     integer :: rhf2 = 0 !< Restricted (1) or unrestricted (2) for 2 wave functions.
-    integer :: nao1 = 0 !< Number of atomic orbitals 1.
-    integer :: nao2 = 0 !< Number of atomic orbitals 2.
-    integer :: nmo1 = 0 !< Number of molecular orbitals 1.
-    integer :: nmo2 = 0 !< Number of molecular orbitals 2.
     integer :: nwf1 = 0 !< Number of wave functions 1.
     integer :: nwf2 = 0 !< Number of wave functions 2.
-    integer :: noa1 = 0 !< Number of alpha occupied orbitals 1.
-    integer :: noa2 = 0 !< Number of alpha occupied orbitals 2.
-    integer :: nva1 = 0 !< Number of alpha virtual orbitals 1.
-    integer :: nva2 = 0 !< Number of alpha virtual orbitals 1.
-    integer :: nob1 = 0 !< Number of beta occupied orbitals 1.
-    integer :: nob2 = 0 !< Number of beta occupied orbitals 2.
-    integer :: nvb1 = 0 !< Number of beta virtual orbitals 1.
-    integer :: nvb2 = 0 !< Number of beta virtual orbitals 1.
-    real(dp), allocatable :: mo1(:, :, :) !< Molecular orbital coefficients 1.
-                                          !! Dimensions: nao1 x nmo1 x rhf1.
-    real(dp), allocatable :: mo2(:, :, :) !< Molecular orbital coefficients 2.
-                                          !! Dimensions: nao2 x nmo2 x rhf2.
+    type(ccg), allocatable :: ccg1(:) !< Atomic orbitals 1.
+    type(ccg), allocatable :: ccg2(:) !< Atomic orbitals 2.
+    real(dp), allocatable :: trans1(:, :) !< Transformation matrix for basis functions 1.
+    real(dp), allocatable :: trans2(:, :) !< Transformation matrix for basis functions 2.
+    real(dp), allocatable :: moa1(:, :) !< Molecular orbital coefficients alpha 1.
+    real(dp), allocatable :: moa2(:, :) !< Molecular orbital coefficients alpha 2.
+    real(dp), allocatable :: mob1(:, :) !< Molecular orbital coefficients beta 1.
+    real(dp), allocatable :: mob2(:, :) !< Molecular orbital coefficients beta 2.
     real(dp), allocatable :: wfa1(:, :, :) !< CI alpha single excitation coefficients 1.
-                                           !! Dimensions: nva1 x noa1 x nwf1.
     real(dp), allocatable :: wfa2(:, :, :) !< CI alpha single excitation coefficients 2.
-                                           !! Dimensions: nva2 x noa2 x nwf2.
     real(dp), allocatable :: wfb1(:, :, :) !< CI beta single excitation coefficients 1.
-                                           !! Dimensions: nvb1 x nob1 x nwf1.
     real(dp), allocatable :: wfb2(:, :, :) !< CI beta single excitation coefficients 2.
-                                           !! Dimensions: nvb2 x nob2 x nwf2.
+    logical, allocatable :: occ1(:, :) !< Occupied MO mask 1.
+    logical, allocatable :: occ2(:, :) !< Occupied MO mask 2.
+    logical, allocatable :: act1(:, :) !< Active MO mask 1.
+    logical, allocatable :: act2(:, :) !< Active MO mask 2.
     real(dp), allocatable :: s_ao(:, :) !< Atomic orbital overlaps.
-                                        !! Dimensions: nao1 x nao2.
     real(dp), allocatable :: s_mo(:, :, :) !< Molecular orbital overlaps.
-                                           !! Dimensions: nmo1 x nmo2 x rhf
     real(dp), allocatable :: s_wf(:, :) !< Wave function overlaps.
-                                        !! Dimensions: nwf1 x nwf2.
-    real(dp), allocatable :: wrk(:, :)
-    integer :: i,s,ounit
     character(len=1000) :: temp
+    character(len=:), allocatable :: input_format
+    character(len=:), allocatable :: dir1
+    character(len=:), allocatable :: dir2
     real(dp) :: thr
+    integer :: i
+    integer :: ounit
 
-    i = command_argument_count()
-    thr = 1.0_dp
-    if (i > 0) then
-        call get_command_argument(1, temp)
-        read(temp, *) thr
-    end if
+    input_format = 'turbomole'
+    write(stdout, *) 'Path to input for bra states:'
+    read(stdin, '(a)') temp
+    dir1 = trim(adjustl(temp))
+    write(stdout, *) 'Path to input for ket states:'
+    read(stdin, '(a)') temp
+    dir2 = trim(adjustl(temp))
+    write(stdout, *) 'Threshold for truncating wave functions:'
+    read(stdin, *) thr
 
-    call read_txt('s_ao', nao1, nao2, s_ao)
-    call read_txt('mo1', nao1, nmo1, rhf1, mo1)
-    call read_txt('mo2', nao2, nmo2, rhf2, mo2)
+    call read_ccg_ao(input_format, dir1, ccg1, trans_ao=trans1)
+    call read_ccg_ao(input_format, dir2, ccg2, trans_ao=trans2)
+    call one_el_op(ccg1, ccg2, [0,0,0], trans1, trans2, s_ao)
 
-    call read_txt('wfa1', nva1, noa1, nwf1, wfa1)
-    call read_txt('wfa2', nva2, noa2, nwf2, wfa2)
-    if (rhf1 == 2) call read_txt('wfb1', nvb1, nob1, nwf1, wfb1)
-    if (rhf2 == 2) call read_txt('wfb2', nvb2, nob2, nwf2, wfb2)
+    call read_mo(input_format, dir1, moa_c=moa1, mob_c=mob1)
+    call read_mo(input_format, dir2, moa_c=moa2, mob_c=mob2)
+
+    call read_cis(input_format, dir1, cisa=wfa1, cisb=wfb1, occ_mo=occ1, act_mo=act1, norm=.true.)
+    call read_cis(input_format, dir2, cisa=wfa2, cisb=wfb2, occ_mo=occ2, act_mo=act2, norm=.true.)
+    nwf1 = size(wfa1, 3)
+    nwf2 = size(wfa2, 3)
+    if (allocated(wfb1)) rhf1 = 2
+    if (allocated(wfb2)) rhf2 = 2
     rhf = max(rhf1, rhf2)
     if (rhf1 /= rhf) then
-        allocate(wfb1(nva1, noa1, nwf1))
         wfa1 = wfa1 / sqrt(2.0_dp)
-        wfb1 = -wfa1
+        allocate(mob1, source=moa1)
+        allocate(wfb1, source=wfa1)
+        wfb1 = -wfb1
     else if (rhf2 /= rhf) then
-        allocate(wfb2(nva2, noa2, nwf2))
         wfa2 = wfa2 / sqrt(2.0_dp)
-        wfb2 = -wfa2
+        allocate(mob2, source=moa2)
+        allocate(wfb2, source=wfa2)
+        wfb2 = -wfb2
     end if
+    call sort_mo(occ1(:, 1), act1(:, 1), moa1, remove_inactive = .true.)
+    call sort_mo(occ2(:, 1), act2(:, 1), moa2, remove_inactive = .true.)
+    if (rhf == 2) call sort_mo(occ1(:, 2), act1(:, 2), mob1, remove_inactive = .true.)
+    if (rhf == 2) call sort_mo(occ2(:, 2), act2(:, 2), mob2, remove_inactive = .true.)
 
-    allocate(wrk(nmo1, nao2))
-    allocate(s_mo(nmo1, nmo2, rhf))
-    do s = 1, rhf
-        call gemm(mo1(:, :, min(s, rhf1)), s_ao, wrk, transa='T')
-        call gemm(wrk, mo2(:, :, min(s, rhf2)), s_mo(:, :, s))
-    end do
+    allocate(s_mo(size(moa1, 2), size(moa2, 2), rhf))
+    call mat_ge_mmm(moa1, s_ao, moa2, s_mo(:, :, 1), transa='T')
+    call mat_ge_mmm(mob1, s_ao, mob2, s_mo(:, :, 2), transa='T')
 
     call cis_overlap(thr, s_mo, wfa1, wfa2, wfb1, wfb2, s_wf)
 
