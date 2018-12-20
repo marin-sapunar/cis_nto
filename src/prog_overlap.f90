7a330af58ed9ddce145c2f699d6b6fc7cc6eb1dc
Marin Sapunar <marin.sapunar@gmail.com>
2018-12-20 15:22:29 +0100

    Add CLI for prog_overlap

--- src/prog_overlap.f90
+++ src/prog_overlap.f90
@@ -47,6 +47,7 @@ program cis_olap_test
     character(len=:), allocatable :: input_format
     character(len=:), allocatable :: dir1
     character(len=:), allocatable :: dir2
+    character(len=:), allocatable :: outfile
     real(dp) :: thr
     integer :: i, narg
     integer :: ounit
@@ -61,32 +62,104 @@ program cis_olap_test
     real(dp) :: time00, time0
     real(dp) :: time_io, time_ao, time_mo, time_wf, time_tot
 
-    print_level = 4
     time00 = omp_get_wtime()
 
+    ! Defaults.
+    print_level = 2
+    outfile = 'omat'
     input_format = 'turbomole'
+    alg = 2
+    thr = 10.0_dp
+    norm = .true.
+    orth = .false.
+    orth_omat = .false.
+    phase_omat = .false.
+    center1 = .false.
+    center2 = .false.
+
+    ! CLI
     narg = command_argument_count()
-    if (narg /= 2) stop 'Call program with paths to bra and ket state calculations.'
-    call get_command_argument(1, temp)
+    i = 0
+    do while (i < narg)
+        i = i + 1
+        call get_command_argument(i, temp)
+        if (i == narg - 1) exit
+        if (i == narg) temp = '--help'
+        select case(temp)
+        case('--help')
+            write(stdout, *) 'usage: cis_overlap.exe [optional arguments] dir1 dir2'
+            write(stdout, *)
+            write(stdout, *) 'Calculate overlaps between two sets CIS type wave functions.'
+            write(stdout, *)
+            write(stdout, *) 'positional arguments:'
+            write(stdout, *) '  dir1                  directory containing calculation for bra states      '
+            write(stdout, *) '  dir2                  directory containing calculation for ket states      '
+            write(stdout, *)
+            write(stdout, *) 'optional arguments:'
+            write(stdout, *) '  --help                show this help message and exit                      '
+            write(stdout, *) '  --cis                 use CIS algorithm for calculating overlaps           '
+            write(stdout, *) '                        (very slow, not recommended)                         '
+            write(stdout, *) '  --nto                 use NTO algorithm for calculating overlaps (default) '
+            write(stdout, *) '  --(no-)norm-states    renormalize input states before calculation          '
+            write(stdout, '(25x,a,l)') 'default: ', norm
+            write(stdout, *) '  --(no-)orth-states    reorthogonalize input states before calculation      '
+            write(stdout, '(25x,a,l)') 'default: ', orth
+            write(stdout, *) '  --(no-)orth-overlap   orthogonalize overlap matrix                         '
+            write(stdout, '(25x,a,l)') 'default: ', orth_omat
+            write(stdout, *) '  --(no-)phase-overlap  match phase between assigned bra/ket states          '
+            write(stdout, '(25x,a,l)') 'default: ', phase_omat
+            write(stdout, *) '  --(no-)recenter-aos   attempt to remove effect of basis set translation by '
+            write(stdout, *) '                        recentering pairs of AOs in AO overlap calculation   '
+            write(stdout, *) '                        (untested, not recommended without further testing)  '
+            write(stdout, '(25x,a,l)') 'default: ', center2
+            write(stdout, *) '  --threshold t         truncate wave functions using given threshold        '
+            write(stdout, *) '  --outfile file        output final overlap matrix to file                  '
+            write(stdout, '(25x,a,a)') 'default: ', outfile
+            write(stdout, *) '  --print-level p       control output level of program (0 = quiet)           '
+            write(stdout, '(25x,a,i0)') 'default: ', print_level
+            stop
+        case('--cis')
+            alg = 1
+        case('--nto')
+            alg = 2
+        case('--norm-states')
+            norm = .true.
+        case('--no-norm-states')
+            norm = .false.
+        case('--orth-states')
+            orth = .true.
+        case('--no-orth-states')
+            orth = .false.
+        case('--orth-overlap')
+            orth_omat = .true.
+        case('--no-orth-overlap')
+            orth_omat = .false.
+        case('--match-phase')
+            phase_omat = .true.
+        case('--no-match-phase')
+            phase_omat = .false.
+        case('--recenter-aos')
+            center2 = .true.
+        case('--no-recenter-aos')
+            center2 = .false.
+        case('--threshold')
+            i = i + 1
+            call get_command_argument(i, temp)
+            read(temp, *) thr
+        case('--outfile')
+            i = i + 1
+            call get_command_argument(i, temp)
+            outfile = trim(adjustl(temp))
+        case('--print-level')
+            i = i + 1
+            call get_command_argument(i, temp)
+            read(temp, *) print_level
+        end select
+    end do
+    call get_command_argument(narg-1, temp)
     dir1 = trim(adjustl(temp))
-    call get_command_argument(2, temp)
+    call get_command_argument(narg, temp)
     dir2 = trim(adjustl(temp))
-    write(stdout, *) 'Algorithm for overlap calculation (1 - CIS, 2 - NTO):'
-    read(stdin, *) alg
-    write(stdout, *) 'Threshold for truncating wave functions:'
-    read(stdin, *) thr
-    write(stdout, *) 'Normalize wave functions before overlap calculation:'
-    read(stdin, *) norm
-    write(stdout, *) 'Orthogonalize wave functions before overlap calculation:'
-    read(stdin, *) orth
-    write(stdout, *) 'Orthogonalize overlap matrix after calculation:'
-    read(stdin, *) orth_omat
-    write(stdout, *) 'Match phase of states after calculation:'
-    read(stdin, *) phase_omat
-    write(stdout, *) 'Remove translation from diagonal blocks of atomic orbital overlap matrix:'
-    read(stdin, *) center1
-    write(stdout, *) 'Remove translation from atomic orbital overlap matrix:'
-    read(stdin, *) center2
 
     ! Read input.
     time0 = omp_get_wtime()
@@ -245,9 +318,9 @@ program cis_olap_test
         end if
     end if
 
-    open(newunit=ounit, file='omat', action='write')
+    open(newunit=ounit, file=outfile, action='write')
     do i = 0, nwf2
-        write(ounit,*) s_wf(:, i)
+        write(ounit, '(1000es24.16)') s_wf(i, :)
     end do
     close(ounit)
 
