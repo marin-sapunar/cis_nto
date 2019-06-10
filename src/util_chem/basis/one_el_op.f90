5060bf5f4f43a369f58e91db7233e2ec286486de
Marin Sapunar <marin.sapunar@gmail.com>
2019-06-10 15:28:03 +0200

    Add option to change operator

--- src/util_chem/basis/one_el_op.f90
+++ src/util_chem/basis/one_el_op.f90
@@ -24,7 +24,10 @@ contains
     !> @details
     !! Calculates a matrix with elements:
     !!
-    !!     S_12(i,j) = < B_1,i | x^lx*y^ly*z^lz | B_2,j >
+    !!     S_12(i,j) = < B_1,i | op(x, y, z) | B_2,j >
+    !!
+    !! The operator to be calculated is given in the operator string either using a named operator
+    !! or an analytic expression (eg: op = "x^1 * y^1 + z^2", see read_operator_string subroutine).
     !!
     !! The matrix is initially constructed in the basis of contracted Cartesian Gaussian functions
     !! ordered by 'internal' order (defined by functions given in ang_mom_defs). Afterwards, the
@@ -35,7 +38,7 @@ contains
     !! is .true., overlaps between functions on two atoms are calculated as if the midpoint between
     !! the two atoms is at the origin.
     !----------------------------------------------------------------------------------------------
-    subroutine one_el_op(geom1, geom2, bs1, bs2, lxyz, mat, center_atoms, center_pairs)
+    subroutine one_el_op(geom1, geom2, bs1, bs2, operator_string, mat, center_atoms, center_pairs)
         use linalg_wrapper_mod, only : gemm
         use basis_set_mod
         use basis_transform_mod
@@ -43,30 +46,123 @@ contains
         real(dp), intent(in) :: geom2(:) !< Geometry 2.
         type(basis_set), intent(inout) :: bs1 !< Basis set 1.
         type(basis_set), intent(inout) :: bs2 !< Basis set 2.
-        integer, intent(in) :: lxyz(3) !< List of exponents of the coordinates in the operator.
+        character(len=*), intent(in) :: operator_string !< Operator to calculate.
         real(dp), allocatable, intent(out) :: mat(:, :) !< Operator matrix.
         logical, intent(in), optional :: center_atoms !< Translate same atom to center.
         logical, intent(in), optional :: center_pairs !< Translate pairs of atoms to center.
+        real(dp), allocatable :: tmp(:, :)
+        integer, allocatable :: lxyz(:, :)
+        integer, allocatable :: add(:)
+        integer :: clxyz(3), i
         logical :: opt_atoms
         logical :: opt_pairs
         type(basis_transform) :: trans1
         type(basis_transform) :: trans2
+        character(len=:), allocatable :: tstring
 
         opt_atoms = .false.
         opt_pairs = .false.
         if (present(center_atoms)) opt_atoms = center_atoms
         if (present(center_pairs)) opt_pairs = center_pairs
 
+        select case(operator_string)
+        case('overlap')
+            tstring = 'x^0'
+        case('r^2')
+            tstring = 'x^2 + y^2 + z^2'
+        case default
+            tstring = operator_string
+        end select
+        call read_operator_string(tstring, lxyz, add)
+
         ! Calculate matrix in basis of Cartesian GTOs in default order.
-        call cart_one_el_op(geom1, geom2, bs1, bs2, lxyz, mat, opt_atoms, opt_pairs)
+        do i = 1, size(lxyz, 1)
+            clxyz = lxyz(i, :)
+            call cart_one_el_op(geom1, geom2, bs1, bs2, clxyz, tmp, opt_atoms, opt_pairs)
+            if (.not. allocated(mat)) allocate(mat(1:size(tmp, 1), 1:size(tmp, 2)), source=0.0_dp)
+            if (add(i) == 1) then
+                mat = mat + tmp
+            else
+                mat = mat - tmp
+            end if
+        end do
 
         ! Convert to original basis format.
         call trans1%init(bs1, 'internal', bs1%source_format)
         call trans2%init(bs2, 'internal', bs2%source_format)
         call trans1%transform(mat, 1)
         call trans2%transform(mat, 2)
-
     end subroutine one_el_op
+
+
+    !----------------------------------------------------------------------------------------------
+    ! SUBROUTINE: read_operator_string
+    !
+    !> @brief Read operator from analytical expression.
+    !> @details
+    !! Currently, the reader is limited to additions, subtractions and multiplications of terms
+    !! of the form q^l, where l must be an integer. Multiplications have to be explicitly written.
+    !! Examples of allowed operators:
+    !!    x
+    !!    x^2
+    !!    x^2 + y^2
+    !!    x^2 + y^2 * z^2
+    !!    x^2 + y^2 * z^2 + ...
+    !----------------------------------------------------------------------------------------------
+    subroutine read_operator_string(operator_string, lxyz, add)
+        use string_mod, only: string, string_parse
+        character(len=*), intent(in) :: operator_string !< Operator to calculate.
+        integer, allocatable :: lxyz(:, :) !< Exponents for the components of the operator.
+        integer, allocatable, intent(out) :: add(:) !< Add (1) or subtract (2) each component.
+        type(string), allocatable :: add_substrings(:)
+        type(string), allocatable :: mult_substrings(:)
+        type(string), allocatable :: exp_substrings(:)
+        integer, allocatable :: found_signs(:)
+        integer :: add_narg, mult_narg, exp_narg
+        integer :: i, j, k, pow, chk
+ 
+        call string_parse(operator_string, '+-', add_narg, add_substrings, found_signs)
+        allocate(lxyz(add_narg, 3), source=0)
+        allocate(add(add_narg), source=1)
+        if (operator_string(1:1) == '-') add(1) = 2
+        add(2:) = found_signs
+        do i = 1, add_narg
+            call string_parse(add_substrings(i)%s, '*', mult_narg, mult_substrings)
+            do j = 1, mult_narg
+                call string_parse(mult_substrings(j)%s, '^', exp_narg, exp_substrings)
+                select case(exp_substrings(1)%s)
+                case('x')
+                    k = 1
+                case('y')
+                    k = 2
+                case('z')
+                    k = 3
+                case default
+                    if (exp_narg /= 2) then
+                        write(stderr, *) 'Error in read_operator_string. Failed to read base.'
+                        write(stderr, *) '    Substring: ', mult_substrings(j)%s
+                        stop
+                    end if
+                end select
+                select case(exp_narg)
+                case(1)
+                    pow = 1
+                case(2)
+                    read(exp_substrings(2)%s, *, iostat=chk) pow
+                    if (chk /= 0) then
+                        write(stderr, *) 'Error in read_operator_string. Failed to read power.'
+                        write(stderr, *) '    Substring: ', mult_substrings(j)%s
+                        stop
+                    end if
+                case default
+                    write(stderr, *) 'Error in read_operator_string. Failed to parse.'
+                    write(stderr, *) '    Substring: ', mult_substrings(j)%s
+                    stop
+                end select
+                lxyz(i, k) = lxyz(i, k) + pow
+            end do
+        end do
+    end subroutine read_operator_string
 
 
     !----------------------------------------------------------------------------------------------
